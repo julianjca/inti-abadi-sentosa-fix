06346a2ceae0e81193ffe2f2c46faed69292855d
julianjca <julianjca11@gmail.com>
2018-06-20 00:15:55 +0700

    first commit

---
+++ scripts/carandbus.js
@@ -0,0 +1,7 @@
+const car = document.getElementById('car');
+const bus = document.getElementById('bus');
+
+car.addEventListener('click', ()=>{
+  document.getElementById('car').style.display = "none";
+});
+
