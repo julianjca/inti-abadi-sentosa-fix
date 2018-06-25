eee4562383c6e672c9cd673f07c4205cceab52c9
julianjca <julianjca11@gmail.com>
2018-06-25 23:03:30 +0700

    Changing Working Environment

--- scripts/carandbus.js
+++ scripts/carandbus.js
@@ -1,7 +1,17 @@
 const car = document.getElementById('car');
 const bus = document.getElementById('bus');
+const carproduct = document.querySelector('.carproduct');
+const busproduct = document.querySelector('.busproduct');
+
 
 car.addEventListener('click', ()=>{
-  document.getElementById('car').style.display = "none";
+  carproduct.classList.add("active");
+  busproduct.classList.remove("active");
+});
+
+bus.addEventListener('click', ()=>{
+  busproduct.classList.add("active");
+  carproduct.classList.remove("active");
+
 });
 
